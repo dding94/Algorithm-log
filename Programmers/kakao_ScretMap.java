a77c2023170b29474e3059cb9b24b8e0519edadf
dding94 <audrn6689@gmail.com>
2022-01-20 17:22:32 +0900

    비트 연산, String 활용

---
+++ Programmers/kakao_ScretMap.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        answer = new String[n];
+
+
+        for(int i=0; i<n; i++) {
+            answer[i] = Integer.toBinaryString((arr1[i] | arr2[i]));
+        }
+
+        for(int i=0; i<n ;i++) {
+            answer[i] = String.format("%"+ n + "s", answer[i]);
+            answer[i] = answer[i].replace("1", "#");
+            answer[i] = answer[i].replace("0", " ");
+        }
+
+        return answer;
+    }
+}
