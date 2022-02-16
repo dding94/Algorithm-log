3f23072fedf3fa5bf42ee055914165380578c27b
dding94 <audrn6689@gmail.com>
2022-02-16 15:55:55 +0900

    문자열 활용

---
+++ Programmers/kakao_숫자 문자열과 영단어.java
@@ -0,0 +1,39 @@
+//방법 1
+class Solution {
+    public int solution(String s) {
+        String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i  10; i++) {
+            s = s.replaceAll(word[i], String.valueOf((i)));
+        }
+
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+
+//방법 2
+class Solution {
+    public int solution(String s) {
+        String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        StringBuilder sb = new StringBuilder();
+
+        for (int pos = 0; pos < s.length(); ) {
+            if (Character.isDigit(s.charAt(pos))) {
+                sb.append(s.charAt(pos));
+                pos++;
+            }else{
+                for (int i = 0; i < 10; i++) {
+                    if (s.startsWith(word[i], pos )) {
+                        sb.append(i);
+                        pos += word[i].length();
+                        break;
+                    }
+                }
+            }
+        }
+
+        int answer = Integer.parseInt(sb.toString());
+        return answer;
+    }
+}
