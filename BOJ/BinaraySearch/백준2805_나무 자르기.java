c262b550471d843998c6e559409cd25bfa02f08a
Jeong Myeonggu <audrn6689@gmail.com>
2022-03-03 12:26:50 +0900

    2805 복습

--- BOJ/BinaraySearch/백준2805_나무 자르기.java
+++ BOJ/BinaraySearch/백준2805_나무 자르기.java
@@ -1,50 +1,51 @@
-package 백준.PS2월;
+//나무 자르기 - 복습
+package 백준;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class PS0219_2805 {
-
+class test {
     static int[] tree;
-    static int n, m;
-    static long sum;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken()); //target
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         tree = new int[n];
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < n; i++) {
             tree[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(tree);
+
 
         int start = 0;
         int end = tree[n - 1];
-
+        int ans = 0;
+        //높이 기준으로 찾기
         while (start <= end) {
             int mid = (start + end) / 2;
-            sum = 0;
 
-            for (int i : tree) {
-                if (i > mid) {
-                    sum += i - mid;
-                }
-            }
-
-            if (sum >= m) {
+            if(sawTree(mid) >= m){
                 start = mid + 1;
+                ans = mid;
             }else{
                 end = mid - 1;
             }
         }
 
-        System.out.println(end);
+        System.out.println(ans);
+    }
+
+    static int sawTree(int mid) {
+        int sum = 0;
+        for (int x : tree) {
+            if (x > mid) {
+                sum += (x - mid);
+            }
+
+        }
+        return sum;
     }
 }
