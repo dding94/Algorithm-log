a6324059da91f716fb4424289c08fba33d82f38e
Jeong Myeonggu <audrn6689@gmail.com>
2022-03-03 12:00:17 +0900

    1920 복습

--- BOJ/BinaraySearch/백준1920_수 찾기.java
+++ BOJ/BinaraySearch/백준1920_수 찾기.java
@@ -1,16 +1,15 @@
-//수찾기
-package 백준.PS2월;
+//수 찾기 - 복습
+package 백준;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class PS0218_1920 {
+class test {
     static int[] a;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         a = new int[n];
 
@@ -18,41 +17,35 @@ public class PS0218_1920 {
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(a);
 
         int m = Integer.parseInt(br.readLine());
-        int[] b = new int[m];
+        int[] findNum = new int[m];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            b[i] = Integer.parseInt(st.nextToken());
+            findNum[i] = Integer.parseInt(st.nextToken());
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        for (int x : b) {
-            sb.append(search(x, 0, a.length - 1) + "\n");
+        for (int target : findNum) {
+            sb.append(search(0, n - 1, target) + "\n");
         }
-
         System.out.println(sb);
+
     }
 
-    private static int search(int target, int start, int end) {
-        if (start > end) {
-            return 0;
+    static int search(int start, int end, int target) {
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if(a[mid] > target){
+                end = mid - 1;
+            } else if (a[mid] < target) {
+                start = mid + 1;
+            } else {
+                return 1;
+            }
         }
-
-        int mid = (start + end) / 2;
-
-        if (a[mid] == target) {
-            return 1;
-        } else if (a[mid] > target) {
-            return search(target, start, mid -1);
-        } else if (a[mid] < target) {
-            return search(target, mid + 1, end);
-        }
-
         return 0;
     }
 }
