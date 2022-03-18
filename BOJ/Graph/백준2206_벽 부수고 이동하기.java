4c70f398c5f239e77a37f71cdedd09c5f8d766bf
Jeong Myeonggu <audrn6689@gmail.com>
2022-03-18 23:14:42 +0900

    bfs 문제 재풀이

--- BOJ/Graph/백준2206_벽 부수고 이동하기.java
+++ BOJ/Graph/백준2206_벽 부수고 이동하기.java
@@ -1,80 +1,84 @@
-//���� 2206 �� �μ��� �̵��ϱ�
+//벽 부수고 이동하기
+package 백준.PS3월;
+
 import java.io.*;
 import java.util.*;
 
-class Node{
-	int x;
-	int y;
-	int dist;
-	int boom;
-	Pair(int x, int y, int dist, int boom){
-		this.x = x;
-		this.y = y;
-		this.dist = dist;
-		this.boom = boom;
+public class PS0318_2206 {
+	static class Node{
+		int x;
+		int y;
+		int dist;
+		int boom;
+
+		public Node(int x, int y, int dist, int boom) {
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+			this.boom = boom;
+		}
 	}
-}
 
-public class Node{
-
-	static final int dx[] = {0,0,1,-1};
-	static final int dy[] = {1,-1,0,0};
-	static int n,m,ans;
-	static boolean visit[][][];
-	static int map[][];
+	static final int dx[] = {0, 0, 1, -1};
+	static final int dy[] = {1, -1, 0, 0};
+	static int[][] map;
+	static boolean[][][] visit;
+	static int n, m, ans;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		map = new int[n][m];
 		visit = new boolean[n][m][2];
 		ans = -1;
 
-		for(int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			String s = br.readLine();
-			for(int j=0; j<m; j++) {
+			for (int j = 0; j < m; j++) {
 				map[i][j] = s.charAt(j) - '0';
 			}
 		}
+
 		bfs();
-		
+
 		System.out.println(ans);
-			
 	}
-	
-	static void bfs() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0,0,1,0));
+
+	static void bfs(){
+		Queue<백준.PS3월.PS0318_2206.Node> q = new LinkedList<>();
+		q.add(new 백준.PS3월.PS0318_2206.Node(0, 0, 1, 0));
 		visit[0][0][0] = true;
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
+
+		while (!q.isEmpty()) {
+			백준.PS3월.PS0318_2206.Node now = q.poll();
+
 			int x = now.x;
 			int y = now.y;
-			
-			if(x==n-1 && y == m-1) {
+
+			if (x == n - 1 && y == m - 1) {
 				ans = now.dist;
 				return;
 			}
-			
-			for(int k=0; k<4; k++) {
+
+			for (int k = 0; k < 4; k++) {
 				int nx = x + dx[k];
 				int ny = y + dy[k];
-				if(0>nx || nx>=n || 0>ny || ny>=m) continue;
-				
-				if(map[nx][ny] == 0 && !visit[nx][ny][now.boom]) {
-					visit[nx][ny][now.boom] = true;
-					q.add(new Node(nx,ny,now.dist+1,now.boom));
-				}else {
-					if(now.boom == 0 && !visit[nx][ny][now.boom+1]) {
-						visit[nx][ny][now.boom+1] = true;
-						q.add(new Node(nx,ny,now.dist+1,now.boom+1));
+
+				if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+					if (map[nx][ny] == 0 && !visit[nx][ny][now.boom]) {
+						visit[nx][ny][now.boom] = true;
+						q.add(new 백준.PS3월.PS0318_2206.Node(nx, ny, now.dist + 1, now.boom));
+					}else{
+						if (now.boom == 0 && !visit[nx][ny][now.boom + 1]) {
+							visit[nx][ny][now.boom + 1] = true;
+							q.add(new 백준.PS3월.PS0318_2206.Node(nx, ny, now.dist + 1, now.boom + 1));
+						}
 					}
 				}
-				
 			}
 		}
-		
+
 	}
 }
