087b373dfc0540011524541d6b787f1d56bd74c1
Jeong Myeonggu <audrn6689@gmail.com>
2022-03-23 21:35:12 +0900

    dp lcs

---
+++ BOJ/DP/백준9251_LCS.java
@@ -0,0 +1,31 @@
+//LCS
+package 백준.PS3월;
+
+import java.io.*;
+
+public class PS0323_9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String a = br.readLine();
+        String b = br.readLine();
+
+        int n = a.length();
+        int m = b.length();
+
+        int[][] d = new int[n + 1][m + 1];
+        d[0][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    d[i][j] = d[i - 1][j - 1] + 1;
+                } else {
+                    d[i][j] = Math.max(d[i - 1][j], d[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(d[n][m]);
+    }
+}
