b3d79f560f22a696b5ab39e3f88b02baad627c7a
Jeong Myeonggu <audrn6689@gmail.com>
2022-06-07 10:25:52 +0900

    양, 음 두개로 나눈 그리디 문제

---
+++ BOJ/Greedy/백준1461_도서관.java
@@ -0,0 +1,48 @@
+package 백준.PS6월;
+
+import java.io.*;
+import java.util.*;
+
+public class PS0607_1461 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> plusBook = new ArrayList<>();
+        ArrayList<Integer> minusBook = new ArrayList<>();
+
+        int max = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(st.nextToken());
+
+            max = Math.max(max, Math.abs(input));
+
+            if (input > 0) {
+                plusBook.add(input);
+            }else{
+                minusBook.add(Math.abs(input));
+            }
+        }
+
+        Collections.sort(plusBook, Collections.reverseOrder());
+        Collections.sort(minusBook, Collections.reverseOrder());
+
+        int ans = 0;
+        for (int i = 0; i < plusBook.size(); i++) {
+            if (i % m == 0) {
+                ans += (plusBook.get(i) * 2);
+            }
+        }
+
+        for (int i = 0; i < minusBook.size(); i++) {
+            if (i % m == 0) {
+                ans += Math.abs(minusBook.get(i) * 2);
+            }
+        }
+
+        System.out.println(ans - max);
+    }
+}
