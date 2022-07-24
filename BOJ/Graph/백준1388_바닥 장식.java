5f3a57424c9ea5d73ac1059d02c69790039cede8
Jeong Myeonggu <audrn6689@gmail.com>
2022-07-24 22:26:31 +0900

    bfs

---
+++ BOJ/Graph/백준1388_바닥 장식.java
@@ -0,0 +1,88 @@
+package 백준.PS7월;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class PS0724_1388 {
+
+    static int n,m;
+    static char[][] map;
+    static boolean[][] visit;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int ans = 0;
+        map = new char[n][m];
+        visit = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visit[i][j]) {
+                    bfs(i, j);
+                    ans++;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(x, y));
+        visit[x][y] = true;
+
+        while (!q.isEmpty()) {
+            Node now = q.poll();
+            x = now.x;
+            y = now.y;
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (map[x][y] == '-') {
+                        if (map[x][ny] == map[x][y] && !visit[x][ny]) {
+                            q.add(new Node(x, ny));
+                            visit[x][ny] = true;
+                        }
+                    }else {
+                        if (map[nx][y] == map[x][y] && !visit[nx][y]) {
+                            q.add(new Node(nx, y));
+                            visit[nx][y] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private static class Node{
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
