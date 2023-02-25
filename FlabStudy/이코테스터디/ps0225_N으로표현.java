4a2b5c252a478e7cca76e0b93dc3be6f9f7805ce
Jeong Myeonggu <audrn6689@gmail.com>
2023-02-25 21:10:47 +0900

    다이나믹 프로그래밍
    풀이를 정확히 구현을 해야했는데 list 로 set 을 쓴다는 접근은 하지 못함.

---
+++ FlabStudy/이코테스터디/ps0225_N으로표현.java
@@ -0,0 +1,55 @@
+package 에프랩스터디.이코테;
+
+import java.util.*;
+
+public class ps0225_N으로표현 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.solution(12, 5);
+    }
+
+    private static class Solution {
+
+        public int solution(int N, int number) {
+            List<Set<Integer>> countList = new ArrayList<>();
+
+            for (int i = 0; i < 9; i++) {
+                countList.add(new HashSet<>());
+            }
+
+            countList.get(1).add(N); // N을 1개 쓴 값은 N 혼자이다.
+
+            for (int i = 2; i < 9; i++) {
+                Set<Integer> countSet = countList.get(i);
+
+                for (int j = 1; j <= i; j++) {
+                    Set<Integer> preSet = countList.get(j);
+                    Set<Integer> postSet = countList.get(i - j);
+
+                    for (int preNum : preSet) {
+                        for (int postNum : postSet) {
+                            countSet.add(preNum + postNum);
+                            countSet.add(preNum - postNum);
+                            countSet.add(preNum * postNum);
+
+                            if (preNum != 0 && postNum != 0) {
+                                countSet.add(preNum / postNum);
+                            }
+                        }
+                    }
+                }
+
+                countSet.add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            }
+
+            for (Set<Integer> sub : countList) {
+                if (sub.contains(number)) {
+                    return countList.indexOf(sub);
+                }
+            }
+
+            return -1;
+        }
+    }
+}
