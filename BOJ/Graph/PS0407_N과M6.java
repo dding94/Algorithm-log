720124b6e9b28389873f92e2d1161e83af8274d2
Jeong Myeonggu <audrn6689@gmail.com>
2023-04-07 23:01:27 +0900

    백준 n과 m 6
    어제와 같은 책을 계속 읽고 있다. 더 많은 책들을 읽어 더 많은 것을 알고싶다.

---
+++ BOJ/Graph/PS0407_N과M6.java
@@ -0,0 +1,51 @@
+package 백준23.PS4월;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class PS0407_N과M6 {
+
+    static int[] numbers;
+    static int[] answer;
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new int[n];
+        answer = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        dfs(0, 0);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int num : answer) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            answer[depth] = numbers[i];
+            dfs(depth + 1, i + 1);
+        }
+    }
+}
